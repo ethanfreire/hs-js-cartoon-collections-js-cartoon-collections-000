7a71cb4ab2038b33b9761afc7d66c141cd0236aa
Ethan Freire <element55star@gmail.com>
2019-03-23 04:58:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 function dwarfRollCall(dwarves) {
+var string = "";
+for(let i = 0 ; i < dwarves.length ; i ++){
+  string += `${[i+1]}. ${dwarves[i]} `;
+}
+return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+for(let i = 0 ; i < planeteerCalls.length ; i++){
+  planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
+}
+return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+  if(words.length === 0 ){
+    return null;
+  }
+for(let i = 0 ; i < words.length ; i++){
+  if (words.length > 4 ){
+    return true;
+  }else{
+    return false;
+  }
+}
 }
 
 function findTheCheese (foods) {
+for(let i = 0 ; i < foods.length ; i++){
+  if(foods[i] == "cheddar" ){
+    return "cheddar";
+    break;
+  }else if(foods[i] == "gouda"){
+    return "gouda";
+    break;
+  }else if(foods[i] == "camembert"){
+    return "camembert";
+    break;
+  }
+  }
+  return "no cheese!"
 }
