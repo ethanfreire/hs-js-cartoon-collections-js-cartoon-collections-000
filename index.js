c834e5db514831734b68c551db8d65fc17d0e2c3
Ethan Freire <element55star@gmail.com>
2019-04-01 11:58:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 function dwarfRollCall(dwarves) {
-var string = "";
-for(let i = 0 ; i < dwarves.length ; i ++){
-  string += `${[i+1]}. ${dwarves[i]} `;
-}
-return string;
+  var string = "";
+  for(let i = 0 ; i < dwarves.length ; i ++){
+    string += `${[i+1]}. ${dwarves[i]} `;
+  }
+  return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
-for(let i = 0 ; i < planeteerCalls.length ; i++){
-  planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
-}
-return planeteerCalls;
+  for(let i = 0 ; i < planeteerCalls.length ; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
+  }
+  return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
   if(words.length === 0 ){
     return null;
   }
-for(let i = 0 ; i < words.length ; i++){
-if(words[i].length > 4){
-  return true ;
-}else {
-  return false;
-}
-}
-
-function findTheCheese (foods) {
-for(let i = 0 ; i < foods.length ; i++){
-  if(foods[i] == "cheddar" ){
-    return "cheddar";
-  }else if(foods[i] == "gouda"){
-    return "gouda";
-  }else if(foods[i] == "camembert"){
-    return "camembert";
+  for(let i = 0 ; i < words.length ; i++){
+    if(words[i].length > 4){
+      return true ;
+    }else {
+      return false;
+    }
   }
+  
+  function findTheCheese (foods) {
+    for(let i = 0 ; i < foods.length ; i++){
+      if(foods[i] == "cheddar" ){
+        return "cheddar";
+      }else if(foods[i] == "gouda"){
+        return "gouda";
+      }else if(foods[i] == "camembert"){
+        return "camembert";
+      }
+    }
+    return "no cheese!";
   }
-  return "no cheese!";
-}
